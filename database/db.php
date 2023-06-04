85a477611b8057175bd45848b2a33586f3de0a20
Federico Airoldi <airoldibusiness@gmail.com>
2023-06-05 00:53:13 +0200

    FASE 3 - riempio i dati delle istanze

--- database/db.php
+++ database/db.php
@@ -13,10 +13,10 @@ include __DIR__ . '/../models/product.php';
 
 
 $prodotti = array(
-    new CategoriaCani('immagine1.jpg', 'Prodotto 1', 10),
-    new CategoriaGatti('immagine2.jpg', 'Prodotto 2', 15),
-    new CategoriaCani('immagine3.jpg', 'Prodotto 3', 20),
-    new CategoriaCani('immagine3.jpg', 'Prodotto 4', 25),
+    new CategoriaCani('https://www.foodpet.it/wp-content/uploads/2018/04/banters-adult-large-chicken-rice.jpg', 'Cibo per cani', 35),
+    new CategoriaGatti('https://www.naturepetshop.it/wp-content/uploads/leonardo-fegato-400-gr-lattine.jpg', 'Cibo per gatti', 25),
+    new CategoriaCani('https://www.peteat.it/3626-large_default/ciotola-da-viaggio-doppia-per-cani-kiwi-walker.jpg', 'Ciotola per cani', 20),
+    new CategoriaCani('https://shop-cdn-m.mediazs.com/bilder/pettorina/per/gatti/trixie/xcat/xl/con/adesivi/e/guinzaglio/2/800/1_457257_xcat_katzengeschirr_mit_leine_und_sticker_fg_dsc1337_2.jpg', 'Guinzaglio per gatti', 25),
     
 );
 
--- views/partials/main.php
+++ views/partials/main.php
@@ -15,7 +15,7 @@ include __DIR__ . '/../../database/db.php';
         <?php foreach($prodotti as $elem) { ?>
 
             <div class="card" style="width: 18rem;">
-                <img src="..." class="card-img-top" alt="...">
+                <img src="<?php echo $elem->immagine ?>" class="card-img-top" alt="...">
                 <div class="card-body">
                     <h5 class="card-title"><?php echo $elem->titolo ?></h5>
                     <p class="card-text">Prezzo: <?php echo $elem->prezzo ?> $</p>
