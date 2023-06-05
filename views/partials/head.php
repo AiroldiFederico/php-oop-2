1c1bc7dcab3290a5dcbde38775b82b1aa4804fb2
Federico Airoldi <airoldibusiness@gmail.com>
2023-06-06 00:54:01 +0200

    FASE 5 - sistemo lo stile del progetto

--- views/partials/head.php
+++ views/partials/head.php
@@ -8,8 +8,8 @@
     <title>PET SHOP</title>
 
     <!-- Style CSS -->
-    <link rel="stylesheet" href="../asset/css/reset.css">
-    <link rel="stylesheet" href="../asset/css/style.css">
+    <link rel="stylesheet" href="./asset/css/style.css">
+    <link rel="stylesheet" href="./asset/css/reset.css">
 
     <!-- Icon style -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.3.0/css/all.min.css" integrity="sha512-SzlrxWUlpfuzQ+pcUCosxcglQRNAq/DZjVsC0lE40xsADsfeQoEypE+enwcOiGjk/bSuGGKHEyjSoQ1zVisanQ==" crossorigin="anonymous" referrerpolicy="no-referrer" />
--- views/partials/main.php
+++ views/partials/main.php
@@ -5,22 +5,22 @@ include __DIR__ . '/../../database/db.php';
 
 ?>
 
-<main class="col-8 m-auto">
+<main class="col-8 m-auto d-flex flex-column align-items-center h-100 ">
 
-    <h1 class="text-center">PET SHOP</h1>
+    <h1 class="text-center mt-4">PET SHOP</h1>
 
 
-    <div class="d-flex  justify-content-center mt-4 gap-2">
+    <div class="d-flex mt-4 gap-2 ">
 
         <?php foreach($prodotti as $elem) { ?>
 
             <div class="card" style="width: 18rem;">
                 <img src="<?php echo $elem->immagine ?>" class="card-img-top" alt="...">
-                <div class="card-body">
+                <div class="card-body position-relative">
                     <h5 class="card-title"><?php echo $elem->titolo ?></h5>
                     <p class="card-text">Prezzo: <?php echo $elem->prezzo ?> $</p>
-                    <a href="#" class="btn btn-primary">ORDINA</a>
-                    <p><?php echo $elem->getIcona() ?></p>
+                    <a href="#" class="btn">ORDINA</a>
+                    <p id="icon"><?php echo $elem->getIcona() ?></p>
                 </div>
             </div>
 
@@ -29,4 +29,5 @@ include __DIR__ . '/../../database/db.php';
         <?php } ?>
 
     </div>
+
 </main>
